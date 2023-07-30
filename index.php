f78c1a22f19aabb90b9ebef12a4ef69220db6068
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-30 17:05:15 +0000

    add all  modification

--- index.php
+++ index.php
@@ -3,9 +3,45 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <? ?> 
+    
+    <?php
+class Utilisateur {
+  private String $nom;
+  private String $Prenom;
+  private String $email;
+
+  public function __construct(String $nom, String $prenom, $email) {
+    $this-> nom = $nom;
+    $this-> prenom = $prenom;
+    $this->email = $email;
+  }
+
+  public function getNom() : String{
+    return $this->nom;
+  }
+
+  public function setNom(String $nom) {
+  $this -> nom = $nom;
+  }
+
+  public function getPrenom() :String {
+  return $this-> prenom
+  }
+
+  public function setPrenom(String $prenom) {
+    $this-> prenom = $prenom;
+  }
+
+  public function getEmail() :String {
+  return $this -> email;
+  }
+
+  public function setEmail(String $email) {
+    $this-> email = $email;
+  }
+}
+
+?> 
 
  
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
-  </body>
 </html>
