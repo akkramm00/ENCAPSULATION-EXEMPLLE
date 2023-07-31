c84b2338786abce09629b74a54342e1227ab5046
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-31 09:53:47 +0000

    add  " __set() section"

--- index.php
+++ index.php
@@ -69,6 +69,44 @@ echo $monChat-> getNom(); // affiche "Minou"
   Dans cet exemple , la propriété 'nom' de la classe "Animal" est définie avec une portée 'protected'.
   Cela signifie que la propriété n'est pas accessible depuis l'extérieur de la classe , mais est accessible par les osus-classes (ici, la classe 'Chat'). Les accesseurs ne sont pas nécéssaires, car la propriété est accessible par la sous-classe.
 </p>
+
+<h2>Fondamental => les Métodes Magiques __get et __set</h2>
+    En php , les métodes magiques __get() et __set() permettent d'accéder et de modifier des propriétés privées et protégées d'une classe depuis l'extérieur de celle-ci.
+    La méthode __get() est appellée automatiquement lorsque l'on tente d'accéder à une propriété inexistante ou inaccéssible depuis l'exterieur de la classe. Elle prend en paramétre le nom de la propriété demandée et doit retourner sa valeur. Cela permet d'accéder à une propriété protégée ou privée d'une classe sans enfreindre l'encapsulation.<br>
+    
+
+    
+<?php
+class MyClass {
+  private $myProperty = 'value';
+
+  public function __get($property) {
+    if(property_exists($this, $property)) {
+      return $this-> $property;
+    }
+  }
+}
+$obj = new MyClass();
+echo $obj->myProperty; // Affiche "value"
+?>
+
+<p>
+  La méthode __set() est appelée automatiquement lorsque l'on tente de modifier une propriété inexistante ou innccéssible depuis l'extérieur de la classe .
+  Elle prend en paramétre le nom de la propriété et sa valeur . Cela permet de modifier une propriété protégée ou privée d'une classe sans enfreindre l'encapsulation.0
+
+  <?php
+ class MyClass {
+   private function __set($property, $value) {
+     if (property_exists($this, $property)) {
+       $this->$property = $value;
+     }
+   }
+ }
+$obj = new Myclass();
+$obj->myProperty = 'new value';
+echo $obj-> myProperty; //Affiche 'new value'
+?>
+</p>
     
   
  
